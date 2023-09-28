96a922498f3ccc2a2189b1d8e2d741b828b9ad35
emathew16 <88091762+emathew16@users.noreply.github.com>
2023-09-28 10:28:06 +0200

    Add files via upload

---
+++ scripts/event.js
@@ -0,0 +1,31 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "./images/cool-emoji.png") {
+    myImage.setAttribute("src", "./images/cool-emoji_original.png");
+  } else {
+    myImage.setAttribute("src", "./images/cool-emoji.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
+
---
+++ scripts/func.js
@@ -0,0 +1,10 @@
+
+
+function multiply(num1, num2) {
+	let result = num1 * num2;
+	return result;
+}
+
+console.log(multiply(4,7));
+console.log(multiply(20,20));
+
